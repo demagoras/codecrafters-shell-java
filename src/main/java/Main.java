8f874711e7fe59f19e0249d748b4eb220696de90
demagoras <tiago.gaspar.bernardino@gmail.com>
2024-12-07 21:20:32 +0000

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-
-        System.out.println(input + ": command not found");
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+        }
     }
 }
