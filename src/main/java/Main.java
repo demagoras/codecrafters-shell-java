132a25875ed177fbcd8a41a51ae6eaa2677a2a94
demagoras <tiago.gaspar.bernardino@gmail.com>
2024-12-07 21:53:53 +0000

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,31 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        Scanner scanner = new Scanner(System.in);
+        List<String> commands = Arrays.asList("echo", "exit", "type");
+
         while (true) {
             System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+            String splitInput = input.substring(5);
 
             if (input.equals("exit 0")) {
                 break;
             }
-            if (input.contains("echo")) {
-                System.out.println(input.substring(5));
+
+            if (input.startsWith("echo")) {
+                System.out.println(splitInput);
+            }
+            else if (input.startsWith("type")) {
+                if (commands.contains(splitInput)) {
+                    System.out.println(splitInput + " is a shell builtin");
+                } else {
+                    System.out.println(splitInput + ": command not found");
+                }
             } else {
                 System.out.println(input + ": command not found");
             }
