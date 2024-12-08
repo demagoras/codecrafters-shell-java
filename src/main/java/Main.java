151167ba9ecd3fb6ddf73fc9ddb8bfc908cedf53
demagoras <tiago.gaspar.bernardino@gmail.com>
2024-12-08 11:11:56 +0000

    ProcessBuilder implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -7,19 +10,26 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        List<String> commandList = Arrays.asList("echo", "exit", "type");
+        List<String> commandList = Arrays.asList("echo", "exit", "type"); // List of possible commands
 
         while (true) {
             System.out.print("$ ");
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
 
+            if (input.isEmpty()) {
+                System.out.println("Please enter a non-empty command.");
+                continue;
+            }
+
+            // Splitting command and arguments
             String command = input.split(" ")[0];
             String splitInput = input.length() > 5 ? input.substring(5) : "";
 
-            if (input.equals("exit 0")) {
+            if (input.equals("exit 0")) { // Exit with code 0
                 break;
             }
 
+            // Check for type of command
             switch (command) {
                 case "echo":
                     System.out.println(splitInput);
@@ -28,7 +38,7 @@ public class Main {
                     type(commandList, splitInput);
                     break;
                 default:
-                    System.out.println(input + ": command not found");
+                    executeExternalCommand(input); // Execute external programs with their own command and argument
             }
         }
     }
@@ -54,6 +64,26 @@ public class Main {
             if (!found) {
                 System.out.println(splitInput + ": not found");
             }
+        }
+    }
+
+    public static void executeExternalCommand(String input) {
+        String[] externalProgram = input.split(" ");
+        try {
+
+            ProcessBuilder processBuilder = new ProcessBuilder(externalProgram); // Execute program
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+
+            BufferedReader bufferedReader = new BufferedReader
+                    (new InputStreamReader(process.getInputStream())); // Process output
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(input + ": command not found");
         }
     }
 }
